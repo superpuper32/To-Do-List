3fc1748d91990fb1697e9e052fa5e30ef481690d
superpuper32 <pochitalin_m@mail.ru>
2024-02-01 22:21:01 +0300

    styles configured

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss'
+import forms from "@tailwindcss/forms"
+
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Inter', 'system-ui','Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+    fontSize: {
+      '3xl': '2.5rem',
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {},
+  },
+  plugins: [
+    forms()
+  ],
+} satisfies Config
+
